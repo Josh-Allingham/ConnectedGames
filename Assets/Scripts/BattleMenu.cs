881f61ced2d7e2258a4de518286dd72c6bed35bd
Joshaa1999 <joshallingham1999@gmail.com>
2026-02-12 16:43:05 +0000

    Minor Chnages
    Minor changes with battle scene, started script for battle scene

---
+++ Assets/Scripts/BattleMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Xml;
+using System;
+
+public class BattleMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
